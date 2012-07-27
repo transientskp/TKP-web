46040a8fd3b26c5cc74136d32bf1d6250ea454c9
Gijs Molenaar <gijs@pythonic.nl>
2012-07-27 14:36:09 +0200

    added missing fields to extracted source view

--- tkpweb/apps/urls.py
+++ tkpweb/apps/urls.py
@@ -6,16 +6,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    # Examples:
     url(r'^database/', include('tkpweb.apps.database.urls', namespace='database')),
     url(r'^dataset/', include('tkpweb.apps.dataset.urls', namespace='dataset')),
     url(r'^account/', include('tkpweb.apps.account.urls', namespace='account')),
-    # url(r'^tkpweb/', include('tkpweb.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('tkpweb.apps.main.urls', namespace='main')),
 )
