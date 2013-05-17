1992bfd913f679322fb35d344fbd3aaa18122222
Tim Staley <tim.staley@soton.ac.uk>
2013-05-17 13:52:35 +0100

    Add 'click to enlarge' toggle for transient thumbnails.

--- tkpweb/apps/dataset/static/dataset/javascript/main.js
+++ tkpweb/apps/dataset/static/dataset/javascript/main.js
@@ -10,4 +10,18 @@ $(function ()
 	{
 	    $("img#sourceplot_" + $(this).attr("number")).css('display', 'none');
 	});
+	
+	$("img.thumbnail").click(
+		function ()
+		{
+			if($(this).attr("clicked")=="0"){
+				$(this).attr("height","100%");
+				$(this).attr("clicked","1")
+			}
+			else{
+				$(this).attr("height","20");
+				$(this).attr("clicked","0")
+			}
+		}
+	);
 });
